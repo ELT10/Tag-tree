c1c02972a38ddd7e34ab75e999fc7c697f19c3e0
Elton Thomas <l10isbig@gmail.com>
2021-08-12 03:25:17 +0530

    Tokensales and Double Fetch
    Added tokensales and fixed double fetch issue

--- src/treecomponent.js
+++ src/treecomponent.js
@@ -3,7 +3,8 @@ import React from "react";
 class TreeComponent extends React.Component {
     state = {
         treeData: {},
-        salesData: {}
+        salesData: {},
+        tokensData: {}
     }
     render() {
         return (
@@ -15,15 +16,18 @@ class TreeComponent extends React.Component {
     }
 
     onAddChild = (val) => {
-        fetch(`https://tagcoin.herokuapp.com/v1/summary/childrenOf/${val}`)
-            .then((response) => response.json())
-            .then((data) => {
-                let st = this.state.treeData;
-                if (!st.hasOwnProperty(data._id)) {
-                    this.setState({ treeData: { ...this.state.treeData, [data._id]: data.children } });
+        if (!this.state.treeData[val]) {
+            fetch(`https://tagcoin.herokuapp.com/v1/summary/childrenOf/${val}`)
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log("fetch");
+                    let st = this.state.treeData;
+                    if (!st.hasOwnProperty(data._id)) {
+                        this.setState({ treeData: { ...this.state.treeData, [data._id]: data.children } });
+                    }
                 }
-            }
-            );
+                );
+        }
     }
 
     fetchsales = (val) => {
@@ -31,21 +35,28 @@ class TreeComponent extends React.Component {
             .then((response) => response.json())
             .then((data) => {
                 let st = this.state.salesData;
+                let td = this.state.tokensData;
                 if (!st.hasOwnProperty(data._id)) {
                     if (data.salesFromChain)
                         this.setState({ salesData: { ...this.state.salesData, [data._id]: data.salesFromChain } });
                     else
                         this.setState({ salesData: { ...this.state.salesData, [data._id]: 0 } });
                 }
+                if (!st.hasOwnProperty(data._id)) {
+                    if (data.tokensPurchased)
+                        this.setState({ tokensData: { ...this.state.tokensData, [data._id]: data.tokensPurchased } });
+                    else
+                        this.setState({ tokensData: { ...this.state.tokensData, [data._id]: 0 } });
+                }
             }
             )
     }
 }
 
-const test = (val, addChild, pro, sales, sfn) => {
+const test = (val, addChild, pro, sales, sfn, tokdata) => {
     return (
         val.map((val) => (
-            <ChildComponent key={val} addChild={addChild} data={pro} id={val} sales={sales} sfn={sfn} />
+            <ChildComponent key={val} addChild={addChild} data={pro} id={val} sales={sales} sfn={sfn} tokdata={tokdata} />
         ))
     )
 }
@@ -56,10 +67,10 @@ const InitComponent = props => {
     return (
         <ul className="firstul">
             <li className="nodeone">
-                <input type="checkbox" id={"ch" + x} value={x} onClick={() => props.addChild(x)} />
-                <label htmlFor={"ch" + x} className="item"><span className="symbol"></span><span className="wadd">{x.slice(0, 5) + '.....' + x.slice(-5)}</span><span className="pcount">{props.state.salesData[x]}</span></label>
+                <input type="checkbox" id={"ch" + x} onClick={(e) => props.addChild(x)} />
+                <label htmlFor={"ch" + x} className="item"><span className="symbol"></span>{props.state.tokensData[x] !== 0 ? <span className="tokencount"> {props.state.tokensData[x]} </span> : ''}<span className="wadd">{x.slice(0, 5) + '.....' + x.slice(-5)}</span><span className="pcount">{props.state.salesData[x]}</span></label>
                 <ul>
-                    {props.state.treeData[x] ? test(props.state.treeData[x], props.addChild, props.state.treeData, props.state.salesData, props.sales) : ''}
+                    {props.state.treeData[x] ? test(props.state.treeData[x], props.addChild, props.state.treeData, props.state.salesData, props.sales, props.state.tokensData) : ''}
                 </ul>
             </li>
         </ul>
@@ -73,10 +84,10 @@ const ChildComponent = props => {
     }
     return (
         <li>
-            <input type="checkbox" id={"ch" + props.id} value={props.id} onClick={() => props.addChild(props.id)} />
-            <label htmlFor={"ch" + props.id} className="item"><span className="symbol"></span><span className="wadd">{props.id.slice(0, 5) + '.....' + props.id.slice(-5)}</span><span className="pcount">{props.sales[props.id]}</span></label>
+            <input type="checkbox" id={"ch" + props.id} onClick={(e) => props.addChild(props.id)} />
+            <label htmlFor={"ch" + props.id} className="item"><span className="symbol"></span><span className="tokencount">{props.tokdata[props.id]}</span><span className="wadd">{props.id.slice(0, 5) + '.....' + props.id.slice(-5)}</span><span className="pcount">{props.sales[props.id]}</span></label>
             <ul>
-                {props.data[props.id] ? test(props.data[props.id], props.addChild, props.data, props.sales, props.sfn) : ''}
+                {props.data[props.id] ? test(props.data[props.id], props.addChild, props.data, props.sales, props.sfn, props.tokdata) : ''}
             </ul>
         </li>
     );
